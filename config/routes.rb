840a3403a06b92b27756519a9f19afe6be414e0f
ERIK TRAUTMAN <erik.trautman@gmail.com>
2014-04-03 21:37:07 -0700

    move home to static pages controller, use webfonts, style navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Codeschoolprep::Application.routes.draw do
 
-  # root 'home.html'
+  root 'static_pages#home'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
